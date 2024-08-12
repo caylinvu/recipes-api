0278cb8a874510a383f9a5cf3efe890f978505f6
Caylin Vu <caylinvu@gmail.com>
2024-08-12 12:46:43 -0500

    Add src folders

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import session from 'express-session';
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import 'dotenv/config';
 
