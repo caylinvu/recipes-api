d2ed5f71b55f5d526710627b46a64844385e5e63
Caylin Vu <caylinvu@gmail.com>
2024-08-13 18:58:49 -0500

    Update controller functions

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -4,33 +4,33 @@ import asyncHandler from 'express-async-handler';
 import { body, validationResult, check } from 'express-validator';
 import fs from 'fs';
 
-// Get all recipe data
-export const getRecipes = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  console.log('Controller function to get all recipes');
-});
-
-// Get data for an individual recipe
-export const getRecipe = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  console.log('Controller function to get an individual recipe');
-});
-
 // Create a new recipe
 export const createRecipe = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log('Controller function to create a recipe');
+    res.send('Controller function to create a recipe');
   },
 );
 
 // Update a recipe
 export const updateRecipe = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log('Controller function to update a recipe');
+    res.send('Controller function to update a recipe');
   },
 );
 
 // Delete a recipe
 export const deleteRecipe = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log('Controller function to delete a recipe');
+    res.send('Controller function to delete a recipe');
   },
 );
+
+// Get data for an individual recipe
+export const getRecipe = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  res.send('Controller function to get an individual recipe');
+});
+
+// Get all recipe data
+export const getRecipes = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  res.send('Controller function to get all recipes');
+});
