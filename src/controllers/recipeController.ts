8a7f9cad46311c7727bedbda90ccabf2e12fe914
Caylin Vu <caylinvu@gmail.com>
2024-08-13 19:30:27 -0500

    Add logic to retrieve recipes from db

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -6,31 +6,37 @@ import fs from 'fs';
 
 // Create a new recipe
 export const createRecipe = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     res.send('Controller function to create a recipe');
   },
 );
 
 // Update a recipe
 export const updateRecipe = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     res.send('Controller function to update a recipe');
   },
 );
 
 // Delete a recipe
 export const deleteRecipe = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     res.send('Controller function to delete a recipe');
   },
 );
 
 // Get data for an individual recipe
-export const getRecipe = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  res.send('Controller function to get an individual recipe');
-});
+export const getRecipe = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const recipe = await Recipe.findById(req.params.recipeId).exec();
+    res.send(recipe);
+  },
+);
 
 // Get all recipe data
-export const getRecipes = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  res.send('Controller function to get all recipes');
-});
+export const getRecipes = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const allRecipes = await Recipe.find().sort({ name: 1 }).exec();
+    res.send(allRecipes);
+  },
+);
