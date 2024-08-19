cc390e26efe76f757aafa9c2865f94f92ad4942e
Caylin Vu <caylinvu@gmail.com>
2024-08-19 15:54:27 -0500

    Update recipe model

--- src/models/recipe.ts
+++ src/models/recipe.ts
@@ -11,35 +11,38 @@ interface Recipe {
   name: string;
   description?: string;
   ingredients: Ingredients[];
-  steps: string[];
+  directions: string[];
   servings?: number;
   notes?: string[];
   source?: string;
-  category: string[];
+  tags: string[];
   image?: string;
 }
 
-const RecipeSchema = new Schema<Recipe>({
-  name: { type: String, required: true },
-  description: { type: String },
-  ingredients: {
-    type: [
-      new Schema<Ingredients>({
-        name: { type: String, required: true },
-        amount: { type: Number },
-        unit: { type: String },
-        prep: { type: String },
-      }),
-    ],
-    required: true,
-    _id: false,
+const RecipeSchema = new Schema<Recipe>(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    ingredients: {
+      type: [
+        new Schema<Ingredients>({
+          name: { type: String, required: true },
+          amount: { type: Number },
+          unit: { type: String },
+          prep: { type: String },
+        }),
+      ],
+      required: true,
+      _id: false,
+    },
+    directions: [{ type: String, required: true }],
+    servings: { type: Number },
+    notes: [{ type: String }],
+    source: { type: String },
+    tags: [{ type: String, required: true }],
+    image: { type: String },
   },
-  steps: [{ type: String, required: true }],
-  servings: { type: Number },
-  notes: [{ type: String }],
-  source: { type: String },
-  category: [{ type: String, required: true }],
-  image: { type: String },
-});
+  { timestamps: true },
+);
 
 export default model<Recipe>('Recipe', RecipeSchema);
