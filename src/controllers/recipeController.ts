9352538e9d6ee2b02d0314800047c4e261e6d122
Caylin Vu <caylinvu@gmail.com>
2024-09-26 21:51:27 -0500

    Add functionality to updateRecipe

--- src/controllers/recipeController.ts
+++ src/controllers/recipeController.ts
@@ -42,11 +42,46 @@ export const createRecipe = [
 ];
 
 // Update a recipe
-export const updateRecipe = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    res.send('Controller function to update a recipe');
-  },
-);
+export const updateRecipe = [
+  body('name', 'Recipe name is required').trim().isLength({ min: 1 }),
+  body('description').optional({ nullable: true }).trim(),
+  body('ingredients', 'Ingredients are required').isArray({ min: 1 }),
+  body('directions', 'Directions are required').isArray({ min: 1 }),
+  body('servings', 'Servings must be an integer').optional({ nullable: true }).trim().isInt(),
+  body('notes', 'Notes must be an array').optional({ nullable: true }).isArray(),
+  body('source').optional({ nullable: true }).trim(),
+  body('tags', 'Must select at least one tag').isArray({ min: 1 }),
+  asyncHandler(async (req: ReqBody<IRecipe>, res: Response, next: NextFunction): Promise<void> => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors.array());
+      return;
+    }
+
+    const updatedRecipe = {
+      name: req.body.name,
+      description: req.body.description,
+      ingredients: req.body.ingredients,
+      directions: req.body.directions,
+      servings: req.body.servings,
+      notes: req.body.notes,
+      source: req.body.source,
+      tags: req.body.tags,
+      image: req.body.image,
+    };
+
+    await Recipe.findByIdAndUpdate(
+      req.params.recipeId,
+      {
+        $set: updatedRecipe,
+      },
+      {},
+    );
+
+    res.send(updatedRecipe);
+  }),
+];
 
 // Delete a recipe
 export const deleteRecipe = asyncHandler(
